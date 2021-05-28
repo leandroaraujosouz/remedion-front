fa8cce6b6ea782f5d8f5526d7a7004ddcb60b170
leandro souza <leandro_a_souza@hotmail.com>
2021-05-28 10:23:05 -0300

    primeiro commit do projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CadastroProdutoComponent } from './cadastro-produto/cadastro-produto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CadastroProdutoComponent
   ],
   imports: [
     BrowserModule,
