2e99665c509bf7ef0f881ff0a8d0f7034cdbad5f
David Santos <69879401+davidsm2k@users.noreply.github.com>
2021-05-28 13:56:02 -0300

    Criando v1.1 do menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CadastroProdutoComponent } from './cadastro-produto/cadastro-produto.component';
 import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CadastroProdutoComponent,
     MenuComponent
   ],
   imports: [
