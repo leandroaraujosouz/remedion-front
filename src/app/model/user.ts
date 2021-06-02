c58e3d0bf0c8db50056228dcc6aa5c5a78ae4b49
Paty-star <80486451+Paty-star@users.noreply.github.com>
2021-06-02 11:24:31 -0300

    Implementação da camada model-user

---
+++ src/app/model/user.ts
@@ -0,0 +1,8 @@
+export class user{
+    public id: number
+    public nomeCompleto: string
+    public email: string
+    public senha: string
+    public foto: string
+    public tipoUsuario: string
+}
