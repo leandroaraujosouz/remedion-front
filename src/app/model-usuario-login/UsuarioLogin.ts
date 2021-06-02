102b7d3a85d26c5fad760a35dce6ebc8bcfd7094
unknown <danieleleal4@gmail.com>
2021-06-02 11:22:44 -0300

    Criação de model UsuarioLogin para o eccomerce remedion

---
+++ src/app/model-usuario-login/UsuarioLogin.ts
@@ -0,0 +1,8 @@
+export class UsuarioLogin{
+    public id: number
+    public nomeCompleto: string
+    public email: string
+    public token: string
+    public foto: string
+    public tipoUsuario: string
+}
