75aa50de18474db15e372776df3b13c747f2b988
unknown <danieleleal4@gmail.com>
2021-06-07 11:33:30 -0300

    criação de model e categoria e elaboração de service categoria

---
+++ src/app/model/Produto.ts
@@ -0,0 +1,14 @@
+import {Categoria} from "./categoria"
+
+export class Produto{
+    public id: number
+    public nome: string
+    public estoque: number
+    public posto: string
+    public municipioCidade: string
+    public zona: string
+    public endereco: string
+    public classificacao: string
+    public ativo: boolean
+    public categoria: Categoria
+}
