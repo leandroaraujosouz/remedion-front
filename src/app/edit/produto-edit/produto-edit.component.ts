25ae20987bad096051336d914e2c2223990ddf81
Jullyane <jullyanedmt@gmail.com>
2021-06-09 13:23:18 -0300

    implementação do componente produto-edit

---
+++ src/app/edit/produto-edit/produto-edit.component.ts
@@ -0,0 +1,88 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Categoria } from 'src/app/model/Categoria';
+import { Produto } from 'src/app/model/Produto';
+import { CategoriaService } from 'src/app/service/categoria.service';
+import { ProdutoService } from 'src/app/service/produto.service';
+import { environment } from 'src/environments/environment.prod';
+
+@Component({
+  selector: 'app-produto-edit',
+  templateUrl: './produto-edit.component.html',
+  styleUrls: ['./produto-edit.component.css']
+})
+export class ProdutoEditComponent implements OnInit {
+
+  fundo: any
+
+  produto: Produto = new Produto()
+  listaProdutos: Produto[]
+  idProduto: number
+
+  categoria: Categoria = new Categoria()
+  listaCategorias: Categoria[]
+  idCategoria: number
+
+  constructor(
+    private router: Router,
+    private categoriaService: CategoriaService,
+    private produtoService: ProdutoService,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit() {
+    window.scroll(0, 0)
+
+    if (environment.token == '') {
+      this.router.navigate(['/entrar'])
+    }
+
+    this.fundo = window.document.querySelector('#fundo')
+    this.mudar()
+    this.findAllCategorias()
+    this.findAllProdutos()
+    this.idProduto = this.route.snapshot.params['id']
+    this.findByIdProduto(this.idProduto)
+
+  }
+
+  mudar() {
+    this.fundo.style.backgroundImage = "url('https://i.imgur.com/wgCvfF7.jpg')"
+  }
+
+  findAllProdutos() {
+    this.produtoService.getAllProdutos().subscribe((resp: Produto[]) => {
+      this.listaProdutos = resp
+    })
+  }
+
+  findByIdProduto(id: number) {
+    this.produtoService.getByIdProduto(id).subscribe((resp: Produto) => {
+      this.produto = resp
+    })
+  }
+
+  findByIdCategoria() {
+    this.categoriaService.getByIdCategoria(this.idCategoria).subscribe((resp: Categoria) => {
+      this.categoria = resp
+    })
+  }
+
+  findAllCategorias() {
+    this.categoriaService.getAllCategoria().subscribe((resp: Categoria[]) => {
+      this.listaCategorias = resp
+    })
+  }
+
+  atualizarProduto() {
+    this.produto.categoria = this.categoria
+    this.produtoService.putProduto(this.produto).subscribe((resp: Produto) => {
+      this.produto = resp
+      alert('Produto atualizado com sucesso!')
+      this.produto = new Produto()
+      this.router.navigate(['/'])
+    })
+
+  }
+
+}
