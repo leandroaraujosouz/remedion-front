1ac0a2ec41fec17974661e4ae7eadc734945ad36
unknown <danieleleal4@gmail.com>
2021-06-09 11:04:00 -0300

    Commit cadastro-produto com ts e html

--- src/app/cadastro-produto/cadastro-produto.component.ts
+++ src/app/cadastro-produto/cadastro-produto.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
+import { Categoria } from '../model/Categoria';
+import { Produto } from '../model/Produto';
+import { CategoriaService } from '../service/categoria.service';
+import { ProdutoService } from '../service/produto.service';
 
 @Component({
   selector: 'app-cadastro-produto',
@@ -10,8 +14,17 @@ import { environment } from 'src/environments/environment.prod';
 export class CadastroProdutoComponent implements OnInit {
 
   fundo: any
+
+  produto: Produto = new Produto()
+  listaProdutos: Produto[]
+
+  categoria: Categoria = new Categoria()
+  listaCategorias: Categoria[]
+  idCategoria: number
   constructor(
-    private router: Router
+    private router: Router,
+    private categoriaService: CategoriaService,
+    private produtoService: ProdutoService
   ) { }
 
   ngOnInit() {
@@ -20,8 +33,39 @@ export class CadastroProdutoComponent implements OnInit {
     }
     this.fundo = window.document.querySelector('#fundo')
     this.mudar()
+    this.findAllCategorias()
+    this.findAllProdutos()
   }
   mudar(){
     this.fundo.style.backgroundImage = "url('http://edivaldojunior.com.br/wp-content/uploads/2018/03/14-12.jpg')"
+  }
+
+  findAllProdutos(){
+    this.produtoService.getAllProdutos().subscribe((resp: Produto[])=>{
+      this.listaProdutos = resp
+    })
+  }
+
+  findByIdCategoria(){
+    this.categoriaService.getByIdCategoria(this.idCategoria).subscribe((resp: Categoria)=>{
+      this.categoria = resp
+    })
+  }
+
+  findAllCategorias(){
+    this.categoriaService.getAllCategoria().subscribe((resp: Categoria[])=>{
+      this.listaCategorias = resp
+    })
+  }
+
+  cadastrarProduto(){
+    this.produto.categoria = this.categoria
+    this.produto.ativo = true
+    this.produtoService.postProduto(this.produto).subscribe((resp: Produto)=>{
+      this.produto = resp
+      alert('Produto cadastrado com sucesso!')
+      this.produto = new Produto()
+      this.findAllProdutos()
+    })
   }
 }
